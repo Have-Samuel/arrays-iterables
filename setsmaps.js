1de19a028366a107d80ee4eb16284b34daf53ceb
Have Samuel <samiehave9@gmail.com>
2022-11-23 23:38:57 +0300

    Working with Maps

--- setsmaps.js
+++ setsmaps.js
@@ -1,10 +1,39 @@
-const ids = new Set(['Hi', 'from', 'Sets']);
-ids.add(2);
-if (ids.has('Hi')) {
-  ids.delete('Hi');
-}
-console.log(ids);
+// const ids = new Set(['Hi', 'from', 'Sets']);
+// ids.add(2);
+// if (ids.has('Hi')) {
+//   ids.delete('Hi');
+// }
+// console.log(ids);
 
-for (const entry of ids.entries()) {
-  console.log(entry[0]);
+// for (const entry of ids.entries()) {
+//   console.log(entry[0]);
+// }
+
+const person1 = { name: 'Have' };
+const person2 = { name: 'Justine' };
+
+const personalData = new Map([[person1, [{ date: 'yesterday', price: 100 }]]]);
+personalData.set(person2, [{ date: 'two weeks ago', price: 500 }]);
+
+console.log(personalData);
+console.log(personalData.get(person1));
+
+// Three ways oays of output
+for (const entry of personalData.entries()) { // => 1
+  console.log(entry);
 }
+
+for (const [key, value] of personalData.entries()) { // => 2
+  console.log(key, value);
+}
+
+for (const key of personalData.keys()) { // => 3
+  console.log(key);
+}
+
+for (const value of personalData.values()) { // => 2
+  console.log(value);
+}
+
+// Shows how many items we have i the map
+console.log(personalData.size);
