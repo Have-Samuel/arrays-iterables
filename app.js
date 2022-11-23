bd45e46a871e2b0bd8a3a036c84668d34aba94ad
Have Samuel <samiehave9@gmail.com>
2022-11-23 18:03:56 +0300

    Adding and Removing data

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
-const numbers = [1, 2, 3];
-console.log(numbers);
+// const numbers = [1, 2, 3];
+// console.log(numbers);
 
-// const moreNumbers = Array(5, 2);
-// console.log(moreNumbers);
+// // const moreNumbers = Array(5, 2);
+// // console.log(moreNumbers);
 
-// const yetMoreNumbers = Array.of(1, 2);
-// console.log(yetMoreNumbers);
+// // const yetMoreNumbers = Array.of(1, 2);
+// // console.log(yetMoreNumbers);
 
-const listItems = document.querySelectorAll('li');
-console.log(listItems);
+// const listItems = document.querySelectorAll('li');
+// console.log(listItems);
 
-const arrayListItems = Array.from(listItems);
-console.log(arrayListItems);
+// const arrayListItems = Array.from(listItems);
+// console.log(arrayListItems);
 
-const hobbies = ['Cooking', 'Reading'];
-const personalData = [30, 'Have', { moreDetail: [] }];
+// const hobbies = ['Cooking', 'Reading'];
+// const personalData = [30, 'Have', { moreDetail: [] }];
 
-const analyticsData = [[1, 3, 5], [8, 9, 30]];
+// const analyticsData = [[1, 3, 5], [8, 9, 30]];
 
-for (const data of analyticsData) {
-  for (dataPoint of data) {
-    console.log(dataPoint);
-  }
-}
+// for (const data of analyticsData) {
+//   for (dataPoint of data) {
+//     console.log(dataPoint);
+//   }
+// }
 
-console.log(personalData[2]);
+// console.log(personalData[2]);
+
+const hobbies = ['Cooking', 'Popping'];
+hobbies.unshift('Knitting');
+hobbies.push('Dancing');
+hobbies.pop();
+hobbies.shift();
+console.log(hobbies);
+
+hobbies[0] = 'Mopping';
+console.log(hobbies);
