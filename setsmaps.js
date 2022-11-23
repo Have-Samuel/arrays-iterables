3525967460491e9e2af33050b6cbe72bb9783282
Have Samuel <samiehave9@gmail.com>
2022-11-23 23:53:31 +0300

    Working with WeakSets

--- setsmaps.js
+++ setsmaps.js
@@ -9,31 +9,40 @@
 //   console.log(entry[0]);
 // }
 
-const person1 = { name: 'Have' };
-const person2 = { name: 'Justine' };
+// const person1 = { name: 'Have' };
+// const person2 = { name: 'Justine' };
 
-const personalData = new Map([[person1, [{ date: 'yesterday', price: 100 }]]]);
-personalData.set(person2, [{ date: 'two weeks ago', price: 500 }]);
+// const personalData = new Map([[person1, [{ date: 'yesterday', price: 100 }]]]);
+// personalData.set(person2, [{ date: 'two weeks ago', price: 500 }]);
 
-console.log(personalData);
-console.log(personalData.get(person1));
+// console.log(personalData);
+// console.log(personalData.get(person1));
 
-// Three ways oays of output
-for (const entry of personalData.entries()) { // => 1
-  console.log(entry);
-}
+// // Three ways oays of output
+// for (const entry of personalData.entries()) { // => 1
+//   console.log(entry);
+// }
 
-for (const [key, value] of personalData.entries()) { // => 2
-  console.log(key, value);
-}
+// for (const [key, value] of personalData.entries()) { // => 2
+//   console.log(key, value);
+// }
 
-for (const key of personalData.keys()) { // => 3
-  console.log(key);
-}
+// for (const key of personalData.keys()) { // => 3
+//   console.log(key);
+// }
 
-for (const value of personalData.values()) { // => 2
-  console.log(value);
-}
+// for (const value of personalData.values()) { // => 2
+//   console.log(value);
+// }
 
-// Shows how many items we have i the map
-console.log(personalData.size);
+// // Shows how many items we have i the map
+// console.log(personalData.size);
+
+let person = { name: 'Have' };
+
+const persons = new WeakSet();
+persons.add(person);
+
+// ...some operations
+person = null;
+console.log(persons);
